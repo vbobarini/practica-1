9ab9fb4e2ad177ea127810dfcd8aa914a7511a17
Cecilia <vbobarini@gmail.com>
2021-10-24 22:41:32 -0300

    modificacion

--- front/productos/images/entrada.js
+++
@@ -1,8 +0,0 @@
-var nombreusuario = prompt('Ingresa tu nombre')
-var cliente = prompt ('¿Sos mayor de 18?')
-var fragancias = ('¿Que tenes ganas de pedir ?')
-var cantidad = prompt ('¿Ya elegiste tu combo?')
-var direccion = prompt ('¿A que direccion te llevamos el pedido?')
-var aclaracion = prompt ('¿Tenes alguna consulta antes de confirmar?')
-
-alert ('Bienvenid@' +nombreusuario)
